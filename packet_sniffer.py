16eea957face7d596223a8ae5eb10914d31f30a1
Jad Al-Awar <155490081+JadAl-Awar@users.noreply.github.com>
2024-05-31 12:05:44 +0300

    Add files via upload

---
+++ packet_sniffer.py
@@ -0,0 +1,58 @@
+"""
+#!/usr/bin/env python
+import scapy.all as scapy
+import argparse
+from scapy.layers import http
+def get_interface():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Specify interface on which to sniff packets")
+    arguments = parser.parse_args()
+    return arguments.interface
+
+def sniff(iface):
+    scapy.sniff(iface=iface, store=False, prn=process_packet)
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print("[+] Http Request >> " + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keys = ["username", "password", "pass", "email"]
+            for key in keys:
+                if key in load:
+                    print("\n\n\n[+] Possible password/username >> " + load + "\n\n\n")
+                    break
+
+iface = get_interface()
+sniff(iface)
+"""
+#!/usr/bin/env python
+import scapy.all as scapy
+import argparse
+from scapy.layers import http
+
+def get_interface():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Specify interface on which to sniff packets")
+    arguments = parser.parse_args()
+    return arguments.interface
+
+def sniff(iface):
+    scapy.sniff(iface=iface, store=False, prn=process_packet)
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        # Decode the Host and Path from bytes to string
+        host = packet[http.HTTPRequest].Host.decode()
+        path = packet[http.HTTPRequest].Path.decode()
+        print("[+] Http Request >> " + host + path)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load.decode(errors='ignore')  # decode bytes to string, ignore errors
+            keys = ["username", "password", "pass", "email"]
+            for key in keys:
+                if key in load:
+                    print("\n\n\n[+] Possible password/username >> " + load + "\n\n\n")
+                    break
+
+iface = get_interface()
+sniff(iface)
